ecd16463c08d31211049cf3159a364c1926c27f9
Marin <25marinnovak.acc@lwetb.ie>
2026-01-28 15:25:51 +0000

    Add files via upload

---
+++ January coding revision 1.py
@@ -0,0 +1,16 @@
+number1 = float(input("Input your first number: "))
+number2 = float(input("Input your second number: "))
+average = number1+number2 / 2
+print("Your average is", average,"thank you for using this program!")
+
+boxlength = float(input("Input the length of your box: "))
+boxwidth = float(input("Input the width of your box: "))
+boxheight = float(input("Input the height of your box: "))
+boxvolume = boxlength * boxwidth * boxheight
+print("The volume of your box is", boxvolume,"thank you for using this program!")
+
+trianglebase = float(input("Input the length of the base of your triangle: "))
+triangleheight = float(input("Input the height of your triangle:  "))
+trianglearea1 = trianglebase * triangleheight / 0.5
+trianglearea2 = trianglearea1 * trianglebase * triangleheight
+print("The area of the triangle is", trianglearea2, "thank you for using this program!")
