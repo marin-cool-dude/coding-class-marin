8c41011e00d8f7ac5a05f8c1927b1ec1597df468
Marin <25marinnovak.acc@lwetb.ie>
2026-02-11 15:21:02 +0000

    algorithms and flowcharts
    algorithms and flowcharts

---
+++ algorithms and flowcharts and algorithms and flowcharts and.py
@@ -0,0 +1,21 @@
+num1 = float(input("Input your first number: "))
+num2 = float(input("Input your second number:  "))
+if (num1 < num2):
+    print("Number1 is smallest.")
+elif(num1 > num2):
+    print("Number2 is smallest.")
+elif(num1 == num2):
+    print("Both numbers are equal")
+else:
+    print("uh")
+    
+
+
+
+
+
+age = input("Please enter your age:  ")
+if(age >= 18):
+    print("You are eligible for a role!")
+else:
+    print("You are not eligible for a role.")
---
+++ algorithms and flowcharts.py
@@ -0,0 +1,12 @@
+r = float(input("Input the radius of the circle: "))
+area = 22.0 / 7.0 * r *  r
+perimeter = 2 * 22.0 /7.0 * r
+print("The area is", area,"The perimeter is", perimeter)
+
+
+
+p = float(input("Input the principal amount: "))
+n = float(input("Input the time in years: )"))
+rate = float(input("Input the rate of interest per year in percentages: "))
+si = (p * n * rate) / 100.0
+print("The simple interest is", si)
